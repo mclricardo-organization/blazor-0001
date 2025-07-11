a5578f0ca1eb6a0220e458065b89741b03f6b103
marcelooliveira <mclricardo@gmail.com>
2025-07-10 23:17:39 -0300

    webassembly atualizado

---
+++ ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -0,0 +1,20 @@
+﻿using ScreenSound.Shared.Modelos.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.WebAssembly.Services
+{
+    public class MusicasAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public MusicasAPI(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("API");
+        }
+
+        public async Task<ICollection<MusicaResponse>> GetMusicasAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+        }
+    }
+}
