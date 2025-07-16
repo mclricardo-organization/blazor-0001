0576618a23305fb59ce8802462fa1886740c227b
marcelooliveira <mclricardo@gmail.com>
2025-07-15 21:00:21 -0300

    logging na main

--- ScreenSound.WebAssembly/Services/MusicasAPI.cs
+++ ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -6,15 +6,29 @@ namespace ScreenSound.WebAssembly.Services
     public class MusicasAPI
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<MusicasAPI> _logger;
 
-        public MusicasAPI(IHttpClientFactory factory)
+        public MusicasAPI(IHttpClientFactory factory, ILogger<MusicasAPI> logger)
         {
             _httpClient = factory.CreateClient("API");
+            _logger = logger;
         }
 
         public async Task<ICollection<MusicaResponse>> GetMusicasAsync()
         {
-            return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+            try
+            {
+                _logger.LogInformation("Tentando buscar música no endpoint de músicas.");
+                var result = await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+                _logger.LogInformation("Busca de músicas concluída.");
+                return result;
+            }
+            catch(Exception e)
+            {
+                string mensagem = "Erro ao buscar músicas da API.";
+                _logger.LogError(e, mensagem);
+                throw new ApplicationException(mensagem, e);
+            }
         }
     }
 }
